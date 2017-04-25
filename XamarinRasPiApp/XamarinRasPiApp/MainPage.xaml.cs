b5d5344a4408a81440c3302f09a96bf38bb7bc01
Dhyey Moliya <Dhyey Moliya>
2017-04-25 18:16:11 +0530

    Initial Commit

---
+++ XamarinRasPiApp/XamarinRasPiApp/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinRasPiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            addressEntry.Completed += (s, e) => portEntry.Focus();
+            continueButton.Clicked += ContinueAsync;
+            resetButton.Clicked += Reset;
+        }
+
+        async void ContinueAsync(object sender, EventArgs e)
+        {
+            showLoading(true);
+            string address = string.Format("http://{0}:{1}", addressEntry.Text, portEntry.Text);
+            //DisplayAlert("Address", address, "OK");
+            //Navigation.PushAsync(new PowerPage());
+            //App.Current.MainPage = new PowerPage(address);
+            //label1.Text = this.address;
+            string url = address + "/check";
+
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                using (
+                    var response = (HttpWebResponse)(
+                        await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, null)
+                    )
+                )
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = new StreamReader(receiveStream);
+                    string str = readStream.ReadToEnd();
+                    dynamic obj = JsonConvert.DeserializeObject(str);
+
+                    if (obj.error == true)
+                    {
+                        // Successful Login
+                        //return true;
+                        showLoading(false);
+                        await DisplayAlert("Error", "Cannot connect.", "OK");
+
+                    }
+                    else
+                    {
+                        // Login Failed
+                        //return false;
+                        showLoading(false);
+
+                        App.Current.MainPage = new SensorsPage(address);
+                    }
+                }
+
+            }
+            catch (Exception exc)
+            {
+                showLoading(false);
+                await DisplayAlert("Error", "Cannot connect.", "OK");
+            }
+
+
+        }
+        void Reset(object sender, EventArgs e)
+        {
+            addressEntry.Text = string.Empty;
+            portEntry.Text = string.Empty;
+
+        }
+        void showLoading(bool status)
+        {
+            loginScreen.IsVisible = !status;
+            loadingScreen.IsVisible = status;
+        }
+    }
+}
---
+++ XamarinRasPiApp/XamarinRasPiApp/SensorsPage.xaml.cs
@@ -0,0 +1,143 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinRasPiApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SensorsPage : ContentPage
+    {
+        string address = "";
+        public SensorsPage()
+        {
+            InitializeComponent();
+
+            //For Development pupose only
+            this.address = "http://192.168.0.5:5000";
+            InitializeButtonEvents();
+            GetInitialValuesAsync();
+        }
+        public SensorsPage(string address)
+        {
+            InitializeComponent();
+            this.address = address;
+            showTempLoading(false);
+            showUltLoading(false);
+            InitializeButtonEvents();
+            GetInitialValuesAsync();
+        }
+        private void GetInitialValuesAsync()
+        {
+            GetTemperatureDataAsync(null, null);
+            GetUltrasonicDataAsync(null, null);
+        }
+        private void InitializeButtonEvents()
+        {
+            getTempButton.Clicked += GetTemperatureDataAsync;
+            getUltrasonicButton.Clicked += GetUltrasonicDataAsync;
+            disconectButton.Clicked += Disconnect;
+        }
+        private void showTempLoading(bool status)
+        {
+            temperatureValue.IsVisible = !status;
+            loadingTempIndicator.IsVisible = status;
+        }
+        private void showUltLoading(bool status)
+        {
+            ultrasonicValue.IsVisible = !status;
+            loadingUltIndicator.IsVisible = status;
+        }
+        private async void GetTemperatureDataAsync(object sender, EventArgs e)
+        {
+            showTempLoading(true);
+            string url = address + "/temperature";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                using (
+                    var response = (HttpWebResponse)(
+                        await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, null)
+                    )
+                )
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = new StreamReader(receiveStream);
+                    string str = readStream.ReadToEnd();
+                    dynamic obj = JsonConvert.DeserializeObject(str);
+
+                    if (obj.value != null)
+                    {
+                        showTempLoading(false);
+                        temperatureValue.Text = Convert.ToDouble(obj.value.ToString()).ToString() + " °C";
+                    }
+                    else
+                    {
+                        showTempLoading(false);
+                        await DisplayAlert("Error", "Error getting temperature value.", "OK");
+                    }
+
+
+                }
+
+            }
+            catch (Exception exc)
+            {
+                showTempLoading(false);
+                await DisplayAlert("Error", "Error getting temperature value.", "OK");
+            }
+        }
+        private async void GetUltrasonicDataAsync(object sender, EventArgs e)
+        {
+            showUltLoading(true);
+            string url = address + "/ultrasonic";
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                using (
+                    var response = (HttpWebResponse)(
+                        await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, null)
+                    )
+                )
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = new StreamReader(receiveStream);
+                    string str = readStream.ReadToEnd();
+                    dynamic obj = JsonConvert.DeserializeObject(str);
+
+                    if (obj.value != null)
+                    {
+                        showUltLoading(false);
+                        ultrasonicValue.Text = Convert.ToDouble(obj.value.ToString()).ToString() + " cm";
+                    }
+                    else
+                    {
+                        showUltLoading(false);
+                        await DisplayAlert("Error", "Error getting ultrasonic value.", "OK");
+                    }
+                }
+
+            }
+            catch (Exception exc)
+            {
+                showUltLoading(false);
+                await DisplayAlert("Error", "Error getting ultrasonic value.", "OK");
+            }
+        }
+        private void Disconnect(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MainPage();
+        }
+    }
+}
