3325183ecf436350f218dd6731d8c14c384d8661
Dhyey Moliya <dhyeymoliya@gmail.com>
2017-05-11 12:12:52 +0530

    Update XamarinAppClient.py

--- raspberrypi-server/XamarinAppClient.py
+++ raspberrypi-server/XamarinAppClient.py
@@ -1,13 +1,28 @@
 import http.client
 import json
+import RPi.GPIO as GPIO
+import dht11
 import time
-from w1thermsensor import W1ThermSensor
+
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+# read data using pin 14
+instance = dht11.DHT11(pin = 4)
 
 while True:
-        for sensor in W1ThermSensor.get_available_sensors():
-                con = http.client.HTTPConnection("raspi.azurewebsites.net")
-                con.request("POST","/api/temperature",body=json.dumps({"tempe$
-                resp = con.getresponse()
-                print(resp.status, resp.reason, resp.read())
-                con.close()
-                time.sleep(0.2)
+	con = http.client.HTTPConnection("raspi.azurewebsites.net")
+	result = instance.read()
+	if result.is_valid():
+		con.request("POST","/api/humidity",body=json.dumps({"humidity":result.humidity}),headers={"access-key":"raspi", "Content-Type":"application/json"})
+		resp = con.getresponse()
+		print(resp.status, resp.reason, resp.read())
+		con.request("POST","/api/temperature",body=json.dumps({"temperature":result.temperature}),headers={"access-key":"raspi", "Content-Type":"application/json"})
+		resp = con.getresponse()
+		print(resp.status, resp.reason, resp.read())
+		#print("Temperature: " + str(result.temperature) + " C")
+		#print("Humidity: " + str(result.humidity))
+	con.close()
+	time.sleep(0.2)
